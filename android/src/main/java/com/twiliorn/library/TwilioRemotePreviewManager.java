blackuy/react-native-twilio-video-webrtc

commit e81a171048c718dcc957e7e5a6c7e6973aa37f6e

adds scaleType prop for rendering participant and local views (#167)
* adds scaleType prop for video rendering

* minor fixes

* add variable declaration

android/src/main/java/com/twiliorn/library/TwilioRemotePreviewManager.java
@@ -24,6 +24,14 @@ public class TwilioRemotePreviewManager extends SimpleViewManager<TwilioRemotePr
         return REACT_CLASS;
     }
 
+    @ReactProp(name = "scaleType")
+    public void setScaleType(TwilioVideoPreview view, @Nullable String scaleType) {
+      if(scaleType.equals('fit')) {
+        view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
+      } else {
+        view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);
+      }
+    }
 
     @ReactProp(name = "trackSid")
     public void setTrackId(TwilioRemotePreview view, @Nullable String trackSid) {

android/src/main/java/com/twiliorn/library/TwilioVideoPreviewManager.java
@@ -17,6 +17,7 @@ import com.facebook.react.uimanager.annotations.ReactProp;
 
 import java.util.Map;
 
+import org.webrtc.RendererCommon;
 
 public class TwilioVideoPreviewManager extends SimpleViewManager<TwilioVideoPreview> {
 
@@ -25,6 +26,15 @@ public class TwilioVideoPreviewManager extends SimpleViewManager<TwilioVideoPrev
     @Override
     public String getName() {
         return REACT_CLASS;
+    }
+
+    @ReactProp(name = "scaleType")
+    public void setScaleType(TwilioVideoPreview view, @Nullable String scaleType) {
+      if(scaleType.equals('fit')) {
+        view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
+      } else {
+        view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);
+      }
     }
 
     @Override

ios/RCTTWLocalVideoViewManager.m
@@ -17,10 +17,13 @@
 
 RCT_EXPORT_MODULE()
 
+RCT_CUSTOM_VIEW_PROPERTY(scalesType, NSInteger, TVIVideoView) {
+  view.subviews[0].contentMode = [RCTConvert NSInteger:json];
+}
+
 - (UIView *)view {
   UIView *container = [[UIView alloc] init];
   TVIVideoView *inner = [[TVIVideoView alloc] init];
-  inner.contentMode = UIViewContentModeScaleAspectFill;
   [container addSubview:inner];
   return container;
 }

ios/RCTTWRemoteVideoViewManager.m
@@ -47,10 +47,13 @@
 
 RCT_EXPORT_MODULE()
 
+RCT_CUSTOM_VIEW_PROPERTY(scalesType, NSInteger, TVIVideoView) {
+  view.subviews[0].contentMode = [RCTConvert NSInteger:json];
+}
+
 - (UIView *)view {
   UIView *container = [[UIView alloc] init];
   TVIVideoView *inner = [[TVIVideoView alloc] init];
-  inner.contentMode = UIViewContentModeScaleAspectFill;
   [container addSubview:inner];
   return container;
 }

src/TwilioVideoLocalView.ios.js
@@ -19,7 +19,8 @@ class TwilioVideoLocalView extends Component {
   }
 
   render () {
-    return <RCTTWLocalVideoView {...this.props}>{this.props.children}</RCTTWLocalVideoView>
+    let scalesType = this.props.scaleType === 'fit' ? 1 : 2
+    return <RCTTWLocalVideoView scalesType={scalesType} {...this.props}>{this.props.children}</RCTTWLocalVideoView>
   }
 }
 

src/TwilioVideoParticipantView.ios.js
@@ -25,7 +25,8 @@ class TwilioVideoParticipantView extends Component {
   }
 
   render () {
-    return <RCTTWRemoteVideoView {...this.props}>{this.props.children}</RCTTWRemoteVideoView>
+    let scalesType = this.props.scaleType === 'fit' ? 1 : 2
+    return <RCTTWRemoteVideoView scalesType={scalesType} {...this.props}>{this.props.children}</RCTTWRemoteVideoView>
   }
 }
 

commit 81f0733da45c098d59edee3bb5776b0f19deb795

Fix scaleType check on android
Fix a bug introduced in #167

android/src/main/java/com/twiliorn/library/TwilioRemotePreviewManager.java
@@ -26,7 +26,7 @@ public class TwilioRemotePreviewManager extends SimpleViewManager<TwilioRemotePr
 
     @ReactProp(name = "scaleType")
     public void setScaleType(TwilioVideoPreview view, @Nullable String scaleType) {
-      if(scaleType.equals('fit')) {
+      if (scaleType.equals("fit")) {
         view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
       } else {
         view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);

android/src/main/java/com/twiliorn/library/TwilioVideoPreviewManager.java
@@ -30,7 +30,7 @@ public class TwilioVideoPreviewManager extends SimpleViewManager<TwilioVideoPrev
 
     @ReactProp(name = "scaleType")
     public void setScaleType(TwilioVideoPreview view, @Nullable String scaleType) {
-      if(scaleType.equals('fit')) {
+      if (scaleType.equals("fit")) {
         view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
       } else {
         view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);
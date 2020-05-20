f4542f3d04d663decd9854d0db9ab87b0be6c30c
steelbrain <i@steelbrain.me>
2020-05-20 16:37:12 +0500

    :bug: Resolve properly in setLocalVideoEnabled in iOS

--- ios/RCTTWVideoModule.m
+++ ios/RCTTWVideoModule.m
@@ -191,29 +191,29 @@ RCT_REMAP_METHOD(setLocalVideoEnabled, enabled:(BOOL)enabled setLocalVideoEnable
   if (self.localVideoTrack != nil) {
     [self.localVideoTrack setEnabled:enabled];
   }
+
   if (self.room == nil) {
     // No room to add/remove tracks from, so ignore.
-    return;
-  }
-
-  if (enabled) {
-    if (self.localVideoTrack != nil) {
-      // Already enabled
-      return;
-    }
-    [self startLocalVideo];
-    if (self.localVideoTrack != nil) {
-      [[self.room localParticipant] publishVideoTrack:self.localVideoTrack];
-    }
   } else {
-    if (self.localVideoTrack == nil) {
-      // Already disabled;
-      return;
+    if (enabled) {
+      if (self.localVideoTrack != nil) {
+        // Already enabled
+      } else {
+        [self startLocalVideo];
+        if (self.localVideoTrack != nil) {
+          [[self.room localParticipant] publishVideoTrack:self.localVideoTrack];
+        }
+      }
+    } else {
+      if (self.localVideoTrack == nil) {
+        // Already disabled;
+      } else {
+        [[self.room localParticipant] unpublishVideoTrack:self.localVideoTrack];
+        [self.camera stopCapture];
+        self.localVideoTrack = nil;
+        self.camera = nil;
+      }
     }
-    [[self.room localParticipant] unpublishVideoTrack:self.localVideoTrack];
-    [self.camera stopCapture];
-    self.localVideoTrack = nil;
-    self.camera = nil;
   }
 
   resolve(@(enabled));

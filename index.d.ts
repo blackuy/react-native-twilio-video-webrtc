423779d259afb6fd8f3b019ed4de6b174d9c7e03
Vladyslav Kochetkov <vladyslavkochetkov1998@gmail.com>
2020-06-26 12:55:49 -0400

    TS: Fix connect params (#337)
    * Fix connect params
    
    * Add Android connect params
    
    Co-authored-by: Vladyslav Kochetkov <me@vladk.dev>

--- index.d.ts
+++ index.d.ts
@@ -75,10 +75,29 @@ declare module "react-native-twilio-video-webrtc"{
     ref?: React.Ref<any>;
   };
 
+  type iOSConnectParams = {
+    accessToken: string;
+    roomName?: string;
+    encodingParameters?: {
+      enableH264Codec?: boolean;
+      // if audioBitrate OR videoBitrate is provided, you must provide both
+      audioBitrate?: number;
+      videoBitrate?: number;
+    }
+  }
+
+  type androidConnectParams = {
+    roomName?: string;
+    accessToken: string;
+    enableAudio?: boolean;
+    enableVideo?: boolean;
+    enableRemoteAudio?: boolean;
+  }
+
   class TwilioVideo extends React.Component<TwilioVideoProps> {
     setLocalVideoEnabled: (enabled: boolean) => Promise<boolean>;
     setLocalAudioEnabled: (enabled: boolean) => Promise<boolean>;
-    connect: (t: { roomName: string; accessToken: string; encodingParameters: object }) => void;
+    connect: (options: iOSConnectParams | androidConnectParams) => void;
     disconnect: () => void;
     flipCamera: () => void;
   }

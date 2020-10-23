2beb366b0890dc5a8fa3391ea8b0090bfff098cf
Jonathan Chang <slycoder@gmail.com>
2020-10-23 11:18:17 -0700

    Revert a few changes for PR cleanliness

--- android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
+++ android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
@@ -164,7 +164,6 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
      */
     private static PatchedVideoView thumbnailVideoView;
     private static LocalVideoTrack localVideoTrack;
-    private boolean isLocalVideoPublished = false;
 
     private static CameraCapturer cameraCapturer;
     private LocalAudioTrack localAudioTrack;
@@ -302,7 +301,7 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
                 /*
                  * If connected to a Room then share the local video track.
                  */
-                if (localParticipant != null && isLocalVideoPublished) {
+                if (localParticipant != null) {
                     localParticipant.publishTrack(localVideoTrack);
                 }
             }
@@ -589,7 +588,6 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
 
     public void publishLocalVideo(boolean enabled) {
         if (localParticipant != null && localVideoTrack != null) {
-            isLocalVideoPublished = enabled;
             if (enabled) {
                 localParticipant.publishTrack(localVideoTrack);
             } else {

3847245ee556ce1ff2a567bad60983f9731a3cea
Umar Nizamani <umar.nizamani@gmail.com>
2020-11-09 16:00:37 +0100

    Improve diff

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module "react-native-twilio-video-webrtc" {
   import { ViewProps } from "react-native";
   import React from "react";
-  
+
   export interface TrackIdentifier {
     participantSid: string;
     videoTrackSid: string;

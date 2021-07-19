158a082e8fcdfce17262f06fc03ebc2c67ee7487
Joshua head <joshua.head@outlook.com>
2021-07-19 13:38:30 +1000

    fix: name of the native module view

--- src/TwilioVideoLocalView2.android.js
+++ src/TwilioVideoLocalView2.android.js
@@ -10,7 +10,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const NativeTwilioLocalPreview = requireNativeComponent(
-  'TwilioLocalView',
+  'RNTwilioLocalView',
   TwilioLocalPreview
 )
 

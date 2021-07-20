306601c081fced222b725471427a7bfc1bbb47a0
Joshua head <joshua.head@outlook.com>
2021-07-20 11:20:19 +1000

    feat: implemented getAvailableLocalTracks

--- android/src/main/java/com/twiliorn/library/TwilioModule.java
+++ android/src/main/java/com/twiliorn/library/TwilioModule.java
@@ -1,7 +1,13 @@
 package com.twiliorn.library;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableArray;
 
 import javax.annotation.Nonnull;
 
@@ -14,14 +20,21 @@ public class TwilioModule extends ReactContextBaseJavaModule {
         return "RNTwilioModule";
     }
 
-    public TwilioModule(ReactContext context) {
+    public TwilioModule(ReactApplicationContext context) {
         super(context);
     }
 
     @ReactMethod
-    public void getAvailableLocalTracks(Promise promise) {    
-        try {         
+    public void getAvailableLocalTracks(Promise promise) {
+        try {
+            String[] tracks = CustomTwilioVideoView.getAvailableLocalTracks();
+            WritableArray writableArray = Arguments.createArray();
 
+            for (String str: tracks) {
+                writableArray.pushString(str);
+            }
+
+             promise.resolve(writableArray);
         } catch(Exception e) {
              promise.reject("Create Event Error", e);    
         }

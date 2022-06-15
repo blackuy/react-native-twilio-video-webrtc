365acf9037f392d06422c8d1d49281c593a0e209
Joshua head <joshua.head@outlook.com>
2022-06-15 20:27:00 +1000

    fix: missing new vd  settings

--- android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
+++ android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
@@ -372,7 +372,7 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
     // ===== SETUP =================================================================================
 
     private VideoFormat buildVideoFormat() {
-        return new VideoFormat(VideoDimensions.HD_720P_VIDEO_DIMENSIONS, 15);
+        return new VideoFormat(new VideoDimensions(640, 360), 15);
     }
 
     private CameraCapturer createCameraCaputer(Context context, String cameraId) {

7571753334196409d5e381ce04e11271598c08f5
Joshua head <joshua.head@outlook.com>
2022-07-07 16:11:32 +1000

    fix: bad import

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-import TwilioVideoLocalView2 from './src/TwilioVideoLocalView2'
-import TwilioVideoLocalView3 from './src/TwilioVideoLocalView3'
-import TwilioVideoLocalView from './src/TwilioVideoLocalView'
-import TwilioVideoParticipantView from './src/TwilioVideoParticipantView'
-import TwilioVideo from './src/TwilioVideo'
-import TwilioModule from './src/TwilioModule'
+import TwilioVideoLocalView from "./src/TwilioVideoLocalView";
+import TwilioVideoParticipantView from "./src/TwilioVideoParticipantView";
+import TwilioVideo from "./src/TwilioVideo";
+import TwilioModule from "./src/TwilioModule";
 
 export {
-  TwilioVideoLocalView2,
-  TwilioVideoLocalView3,
   TwilioVideoLocalView,
   TwilioVideoParticipantView,
   TwilioVideo,
-  TwilioModule
-}
+  TwilioModule,
+};

88684831228d7e499f8de56b2ea298c232641415
JonatanSalas <jonatan.salas@blackbox-vision.tech>
2023-03-17 08:50:42 -0300

    chore: update build folder

--- plugin/build/withIosTwilioVideoWebrtc.js
+++ plugin/build/withIosTwilioVideoWebrtc.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_plugins_1 = require("expo/config-plugins");
-const expo_build_properties_1 = require("expo-build-properties");
 const addPermission = (conf, permission, message) => {
     if (!conf.modResults[permission] && !!message) {
         conf.modResults[permission] = message;
@@ -9,11 +8,11 @@ const addPermission = (conf, permission, message) => {
 };
 const withIosTwilioVideoWebrtc = (config, { cameraPermission, microphonePermission }) => {
     // 1. Deployment target (iOS 11.0+)
-    config = (0, expo_build_properties_1.withBuildProperties)(config, {
-        ios: {
-            deploymentTarget: "11.0",
-        },
-    });
+    // config = withBuildProperties(config, {
+    //  ios: {
+    //    deploymentTarget: "11.0",
+    //  },
+    // });
     // 2. Camera / Microphone Permission on Info.plist
     config = (0, config_plugins_1.withInfoPlist)(config, (conf) => {
         addPermission(conf, "NSCameraUsageDescription", cameraPermission);

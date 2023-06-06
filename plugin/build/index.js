e42f776e15d3673218c38310c9a93ab844451081
Christopher Fields <christopher.fieldsii@streem.com>
2023-06-06 16:53:58 -0500

    revert fix on save for plugin

--- plugin/build/index.js
+++ plugin/build/index.js
@@ -1,25 +1,15 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const config_plugins_1 = require('expo/config-plugins');
-const withAndroidTwilioVideoWebrtc_1 = __importDefault(
-  require('./withAndroidTwilioVideoWebrtc'),
-);
-const withIosTwilioVideoWebrtc_1 = __importDefault(
-  require('./withIosTwilioVideoWebrtc'),
-);
-const pkg = require('../../package.json');
-const withTwilioVideoWebrtc = (config, props) => {
-  config = (0, withAndroidTwilioVideoWebrtc_1.default)(config);
-  config = (0, withIosTwilioVideoWebrtc_1.default)(config, props);
-  return config;
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.default = (0, config_plugins_1.createRunOncePlugin)(
-  withTwilioVideoWebrtc,
-  pkg.name,
-  pkg.version,
-);
+Object.defineProperty(exports, "__esModule", { value: true });
+const config_plugins_1 = require("expo/config-plugins");
+const withAndroidTwilioVideoWebrtc_1 = __importDefault(require("./withAndroidTwilioVideoWebrtc"));
+const withIosTwilioVideoWebrtc_1 = __importDefault(require("./withIosTwilioVideoWebrtc"));
+const pkg = require("../../package.json");
+const withTwilioVideoWebrtc = (config, props) => {
+    config = (0, withAndroidTwilioVideoWebrtc_1.default)(config);
+    config = (0, withIosTwilioVideoWebrtc_1.default)(config, props);
+    return config;
+};
+exports.default = (0, config_plugins_1.createRunOncePlugin)(withTwilioVideoWebrtc, pkg.name, pkg.version);

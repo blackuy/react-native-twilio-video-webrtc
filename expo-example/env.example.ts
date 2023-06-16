55479cdb11b4e82bb04e5577c5425a11039e4324
Christopher Fields <christopher.fieldsii@streem.com>
2023-06-16 12:22:38 -0500

    extracted new example project to separate repo

--- expo-example/env.example.ts
+++
@@ -1,5 +0,0 @@
-// insert twilio access tokens here. use twilio_gen.sh script to generate tokens
-export const TOKENS = ['', ''];
-
-// interval in seconds to log files saved to disk
-export const LOG_FILES_INTERVAL_SEC = 60;
